0f33c0055f0d44226c86902775a095bd7dcb6029
Daniel Card <dan.card@xservus.com>
2017-10-28 18:33:47 +0100

    Update schedule_reboot.ps1

--- schedule_reboot.ps1
+++ schedule_reboot.ps1
@@ -1,7 +1,10 @@
+$servername = "server1"
+$mailserver = "mailserver1"
+
 register-ScheduledJob -Name systemReboot -ScriptBlock {
  
 Restart-Computer -ComputerName $servername -Force -wait
  
-Send-MailMessage -From email@address.com -To email@address.com -Subject "Rebooted" -SmtpServer rcp-sa-exch03.rcp-net.com
+Send-MailMessage -From email@address.com -To email@address.com -Subject "Rebooted" -SmtpServer $mailserver
  
 } -Trigger (New-JobTrigger -At "28/10/2017 8:00pm" -Once) -ScheduledJobOption (New-ScheduledJobOption -RunElevated) -Credential (Get-Credential)
