45cc966db44198814b38a70bea11813af39ff937
Daniel Card <dan.card@xservus.com>
2018-05-14 07:27:17 +0100

    Update Eternalblue_scanner.ps1

--- Eternalblue_scanner.ps1
+++ Eternalblue_scanner.ps1
@@ -1,9 +1,13 @@
-#created by Daniel Card
+#Created by Daniel Card @ Xservus Limited
+#checks for SMB.SRV version as per https://support.microsoft.com/en-ca/help/4023262/how-to-verify-that-ms17-010-is-installed
 #14/05/2018
-#Quick and Dirty Scanner for SMB Version
-#Hotfix enumeration does not always work or multiple reboots are required so created a quick scanning script
 
-$computers = Get-ADComputer -Filter {OperatingSystem -Like 'Windows Server*'}
+
+#search AD for server objects
+$domainserver = Read-Host -Prompt "Please enter name of domain controller to querry"
+$creds = Get-Credential -Message "Please enter credentials for endpoints"
+
+$computers = Get-ADComputer -Credential $creds -Server $domainserver -Filter {OperatingSystem -Like 'Windows Server*'}
 
 
 foreach($computer in $computers){
@@ -11,7 +15,8 @@ foreach($computer in $computers){
 write-host "Name: " $computer.Name
 write-host "LDAP Path: " $computer
 
-$SMBv1Version = Get-WmiObject CIM_Datafile -ComputerName $computer.DNSHostName -Filter "Name='c:\\windows\\system32\\Drivers\\srv.sys'"
+try{
+$SMBv1Version = Get-WmiObject -Credential $creds CIM_Datafile -ComputerName $computer.DNSHostName -Filter "Name='c:\\windows\\system32\\Drivers\\srv.sys'" -ErrorAction Stop
 
 write-host "c:\windows\system32\drivers\srv.sys Version = " $SMBv1Version.Version
 
@@ -36,7 +41,7 @@ write-host "Windows 2012 Identified" -ForegroundColor DarkRed
     if($SMBv1Version.Version.ToString() -lt "6.2.9200.2209"){
 
     write-host "Vulnerable to EternalBlue - Reccomend you patch MS17-010 immediatley" -ForegroundColor Red
-
+    $vulnerablecount = $vulnerablecount + 1
     }
 
 }
@@ -49,7 +54,7 @@ write-host "Windows 2012 R2 or Windowws 8.1 Identified" -ForegroundColor DarkRed
     if($SMBv1Version.Version.ToString() -lt "6.3.9600.18604"){
 
     write-host "Vulnerable to EternalBlue - Reccomend you patch MS17-010 immediatley" -ForegroundColor Red
-
+    $vulnerablecount = $vulnerablecount + 1
     }
 
 }
@@ -62,10 +67,29 @@ write-host "Windows Windows 10 or Server 2016 Identified" -ForegroundColor DarkR
     if($SMBv1Version.Version.ToString() -lt "10.0.10240.17319"){
 
     write-host "Vulnerable to EternalBlue - Reccomend you patch MS17-010 immediatley" -ForegroundColor Red
-
+    $vulnerablecount = $vulnerablecount + 1
     }
 
 }
+
+
+
+
+
+
+
+}
+
+catch{
+write-host "Error Encountered Enumerating SMB Version" -ForegroundColor DarkGreen
+
+}
+finally{
+
+
+
+}
+
 
 
 
